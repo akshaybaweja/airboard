1b0c406b1cb313fd063162385d9612ea0d20fcdd
akshaybaweja <akshaybaweja.1996@gmail.com>
2017-11-08 21:02:54 +0530

    formatting dataset

--- button.py
+++ button.py
@@ -12,6 +12,9 @@ try:
     isPressed = True
     while isPressed:
 	data = ring.get_all_data()
+	accelX, accelY, accelZ = data[0]['x'], data[0]['y'], data[0]['z']
+	gyroX, gyroY, gyroZ = data[1]['x'], data[1]['y'], data[1]['z']
+	print "START "+str(accelX)+" "+str(accelY)+" "+str(accelZ)+" "+str(gyroX)+" "+str(gyroY)+" "+str(gyroZ)+" END"
     	isPressed = not GPIO.input(19)
 except KeyboardInterrupt:
     GPIO.cleanup()       # clean up GPIO on CTRL+C exit
