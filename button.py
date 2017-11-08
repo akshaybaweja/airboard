ef3e6ffe42b9afb333f24b428ee83831bb0286f1
akshaybaweja <akshaybaweja.1996@gmail.com>
2017-11-08 20:54:45 +0530

    minor changes

--- button.py
+++ button.py
@@ -7,19 +7,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 ring = mpu6050(0x69)
 
-isPressed  = False
-
 try:
     GPIO.wait_for_edge(19, GPIO.FALLING)
-    print "Detected. Pressed."
     isPressed = True
     while isPressed:
 	data = ring.get_all_data()
-	accel =  data[0]
-	gyro =  data[1]
-	print accel
-	print gyro
-	print '\n'
     	isPressed = not GPIO.input(19)
 except KeyboardInterrupt:
     GPIO.cleanup()       # clean up GPIO on CTRL+C exit
